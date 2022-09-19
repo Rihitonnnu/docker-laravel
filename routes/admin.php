6277d962a5ae2c59df68d76fdfae15d06120aabb
Rihitonnnu <hexiliang04@gmail.com>
2022-09-19 21:14:54 +0900

    管理側のユーザーに対する操作のルーティング記述

--- routes/admin.php
+++ routes/admin.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +25,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.welcome');
 });
+
+Route::resource('user', UserController::class)->middleware(['auth:admins']);
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
