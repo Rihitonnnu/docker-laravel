962c8df0d8eb57b6e1ec928533cebd6df8bf0fb1
Rihitonnnu <hexiliang04@gmail.com>
2022-10-05 16:25:48 +0900

    不要な記述削除/フォームのタイトル・ボタン追加

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -9,8 +9,13 @@
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
+        <div class="text-center mt-3">
+            <h1 class="text-lg font-bold">管理者ログイン</h1>
+        </div>
+
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
 
         <form method="POST" action="{{ route('admin.login') }}">
             @csrf
@@ -19,36 +24,29 @@
             <div>
                 <x-input-label for="email" :value="__('Email')" />
 
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                    required autofocus />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
                 <x-input-label for="password" :value="__('Password')" />
 
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
             </div>
 
-            <!-- Remember Me -->
+            <!-- Admin Login -->
             <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('user.login') }}">
+                    ユーザーログイン
+                </a>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('admin.password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
 
+            <div class="text-center mt-4">
                 <x-primary-button class="ml-3">
-                    {{ __('Log in') }}
+                    ログイン
                 </x-primary-button>
             </div>
         </form>
--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -9,8 +9,13 @@
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
+        <div class="text-center mt-3">
+            <h1 class="text-lg font-bold">ユーザーログイン</h1>
+        </div>
+
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        
 
         <form method="POST" action="{{ route('user.login') }}">
             @csrf
@@ -32,23 +37,19 @@
                                 required autocomplete="current-password" />
             </div>
 
-            <!-- Remember Me -->
+            <!-- Admin Login -->
             <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('admin.login') }}">
+                        管理者ログイン
+                    </a>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('user.password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
+            <div class="text-center mt-4 ">
+                <button type="button" onclick="location.href='{{route('user.register')}}'" class="ml-3 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150'">
+                    新規登録
+                <button>
                 <x-primary-button class="ml-3">
-                    {{ __('Log in') }}
+                    ログイン
                 </x-primary-button>
             </div>
         </form>
