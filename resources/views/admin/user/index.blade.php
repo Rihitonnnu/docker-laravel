42d2b6faed344e36943a33def02450b491dc1cc1
Rihitonnnu <hexiliang04@gmail.com>
2022-10-06 23:55:16 +0900

    インデント修正

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -15,18 +15,14 @@
                                 <table class="table-auto w-full text-left whitespace-no-wrap">
                                     <thead>
                                         <tr>
-                                            <th
-                                                class="text-center px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
-                                                ID</th>
-                                            <th
-                                                class="text-center px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
-                                                名前</th>
-                                            <th
-                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                            <th class="text-center px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                                                ID
                                             </th>
-                                            <th
-                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                            <th class="text-center px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                                名前
                                             </th>
+                                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"></th>
+                                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"></th>
                                         </tr>
                                     </thead>
                                     @foreach ($users as $user)
@@ -36,21 +32,23 @@
                                                 <td class="px-4 py-3 text-center w-1/4">{{ $user->name }}</td>
                                                 <td class="px-4 py-3 w-1/4">
                                                     <div class="flex pl-4 w-full mx-auto">
-                                                        <button
-                                                            class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">詳細</button>
+                                                        <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                                                            詳細
+                                                        </button>
                                                     </div>
                                                 </td>
                                                 <td class="px-4 py-3 w-1/4">
                                                     <div class="flex pl-4 w-full mx-auto">
-                                                        <button
-                                                            class="flex mx-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">削除</button>
+                                                        <button class="flex mx-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
+                                                            削除
+                                                        </button>
                                                     </div>
                                                 </td>
                                             </tr>
                                         </tbody>
                                     @endforeach
                                 </table>
-                                {{$users->links()}}
+                                {{ $users->links() }}
                             </div>
                         </div>
                     </section>
