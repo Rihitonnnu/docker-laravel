fb2c05bc26bd1352ccb817913dfb85a4cdd70538
Rihitonnnu <hexiliang04@gmail.com>
2022-10-06 23:53:41 +0900

    Eloquentに変更

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class UserController extends Controller
 {
     /** @return \Illuminate\Contracts\View\View*/
     public function index()
     {
-        $users=DB::table('users')->select('id', 'name')->paginate(20);
+        $users=User::paginate(20);
         return view('admin.user.index', compact('users'));
     }
 }
