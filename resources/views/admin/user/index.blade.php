a511848434af7024d072c363c49789c5eec6e693
Rihitonnnu <hexiliang04@gmail.com>
2022-10-16 01:00:23 +0900

    [add] 詳細ボタンにルーティング記載

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -32,7 +32,7 @@
                                                 <td class="px-4 py-3 text-center w-1/4">{{ $user->name }}</td>
                                                 <td class="px-4 py-3 w-1/4">
                                                     <div class="flex pl-4 w-full mx-auto">
-                                                        <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                                                        <button onclick="location.href='{{route('admin.user.show',['user'=>$user->id])}}'" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
                                                             詳細
                                                         </button>
                                                     </div>
