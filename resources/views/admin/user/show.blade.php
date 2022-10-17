0eca2f4fe92861c95eb3b5f9c4b618257ee5a9cb
Rihitonnnu <hexiliang04@gmail.com>
2022-10-17 16:17:25 +0900

    [update] ユーザー詳細画面をテーブル的なUIに変更
    ユーザー詳細画面をインプット的なUIからテーブル的なUIに変更しました。

--- resources/views/admin/user/show.blade.php
+++ resources/views/admin/user/show.blade.php
@@ -10,37 +10,43 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
-                    <section class="text-gray-600 body-font relative">
-                        <div class="container px-5 py-6 mx-auto">
-                            <div class="lg:w-1/2 md:w-2/3 mx-auto">
-                                <div class="flex flex-wrap -m-2">
-                                    <div class="p-2 w-full">
-                                        <div class="relative">
-                                            <label for="name" class="leading-7 text-sm text-gray-600">名前</label>
-                                            <input value="{{$user->name}}" type="text" id="name" name="name" disabled class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                        </div>
-                                    </div>
-                                    <div class="p-2 w-full">
-                                        <div class="relative">
-                                            <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
-                                            <input value="{{$user->email}}" type="email" id="email" name="email" disabled class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                        </div>
-                                    </div>
-                                    <div class="p-2 w-full">
-                                        <div class="relative">
-                                            <label for="created_at" class="leading-7 text-sm text-gray-600">アカウント作成日</label>
-                                            <input value="{{\Carbon\Carbon::parse($user->created_at)->toDateString()}}" type="text" id="created_at" name="created_at" disabled class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                        </div>
-                                    </div>
-
-                                    {{-- 戻るボタン --}}
-                                    <div class="p-2 w-full mt-5">
-                                        <button onclick="location.href='{{ route('admin.user.index') }}'" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">戻る</button>
-                                    </div>
+                    <table class="table-auto w-full text-left whitespace-no-wrap mb-4">
+                            <tr class="border-2 border-gray-300">
+                                <div class="flex">
+                                    <th class="w-1/6 text-center px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                                        ID
+                                    </th>
+                                    <td class="px-4 py-3 text-center w-1/4">{{ $user->id }}</td>
                                 </div>
-                            </div>
-                        </div>
-                    </section>
+                            </tr>
+                            <tr class="border-2 border-gray-300">
+                                <div class="flex">
+                                    <th class="w-1/6 text-center px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                        名前
+                                    </th>
+                                    <td class="px-4 py-3 text-center w-1/4">{{ $user->name }}</td>
+                                </div>
+                            </tr>
+                            <tr class="border-2 border-gray-300">
+                                <div class="flex">
+                                    <th class="w-1/6 text-center px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                        メールアドレス
+                                    </th>
+                                    <td class="px-4 py-3 text-center w-1/4">{{ $user->email }}</td>
+                                </div>
+                            </tr>
+                            <tr class="border-2 border-gray-300">
+                                <div class="flex">
+                                    <th class="w-1/6 text-center px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                        アカウント作成日
+                                    </th>
+                                    <td class="px-4 py-3 text-center w-1/4">{{ \Carbon\Carbon::parse($user->created_at)->toDateString() }}</td>
+                                </div>
+                            </tr>
+                    </table>
+
+                    {{-- 戻るボタン --}}
+                    <x-anchor-button route="{{ route('admin.user.index') }}" title="戻る" color="indigo" />
                 </div>
             </div>
         </div>
