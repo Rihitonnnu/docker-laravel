c0b255a06458fae552aa191acf76efb4fe555ebf
Rihitonnnu <hexiliang04@gmail.com>
2022-10-17 16:08:53 +0900

    [add] ボタンコンポーネント作成
    頻繁に使うボタン(詳細、編集ボタンなど)をaタグを用いてコンポーネント化しました

---
+++ resources/views/components/anchor-button.blade.php
@@ -0,0 +1,3 @@
+<div class="flex pl-4 w-full mx-auto">
+    <a href="{{ $route }}" class="flex mx-auto text-white bg-{{ $color }}-500 border-0 py-2 px-6 focus:outline-none hover:bg-{{ $color }}-600 rounded">{{ $title }}</a>
+</div>
