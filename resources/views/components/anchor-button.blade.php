17389369c30e58d5e18a04e239a5812935b35414
Rihitonnnu <hexiliang04@gmail.com>
2022-10-19 15:03:05 +0900

    [update] aタグボタンコンポーネントのdivタグ削除
    divタグを含むコンポーネントになっていてUIの調整がやりずらいためdivタグを削除しコンポーネントごとにdivタグを書くように修正しました。

--- resources/views/components/anchor-button.blade.php
+++ resources/views/components/anchor-button.blade.php
@@ -1,3 +1,1 @@
-<div class="flex pl-4 w-full mx-auto">
-    <a href="{{ $route }}" class="flex mx-auto text-white bg-{{ $color }}-500 border-0 py-2 px-6 focus:outline-none hover:bg-{{ $color }}-600 rounded">{{ $title }}</a>
-</div>
+<a href="{{ $route }}" class="flex mx-auto justify-center text-white bg-{{ $color }}-500 border-0 py-2 px-6 focus:outline-none hover:bg-{{ $color }}-600 rounded">{{ $title }}</a>
