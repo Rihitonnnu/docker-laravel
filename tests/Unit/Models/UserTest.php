46e22e4d863f7bb3bf929d55e2c05f01ecdf29dc
Rihitonnnu <hexiliang04@gmail.com>
2022-10-31 13:59:38 +0900

    フィクスチャにユーザーのデータ作成を追加/ユーザー削除のテスト追加

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -10,18 +10,27 @@ class UserTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
+        $this->user = User::factory()->create([
+            'name' => 'test',
+            'email' => 'test@example.com'
+        ]);
     }
 
     public function test_updateUser()
     {
-        $user = User::factory()->create([
-            'name' => 'test',
-            'email' => 'test@example.com'
-        ]);
-
-        $updatedUser = (new User())->updateUser('hoge', 'hoge@example.com', $user->id);
+        $updatedUser = (new User())->updateUser('hoge', 'hoge@example.com', $this->user->id);
 
         $this->assertEquals('hoge', $updatedUser->name);
         $this->assertEquals('hoge@example.com', $updatedUser->email);
+    }
+
+    /**
+     * @test
+     */
+    public function test_deleteUser()
+    {
+        $this->user->delete($this->user);
+
+        $this->assertModelMissing($this->user);
     }
 }
