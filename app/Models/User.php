235e7c26f4f828410e8fe50cad27bfd4cd547c62
Rihitonnnu <hexiliang04@gmail.com>
2022-11-01 14:17:42 +0900

    Userモデルでの記事のリレーション定義

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -44,6 +45,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function article(): HasMany
+    {
+        return $this->hasMany(Article::class);
+    }
 
     /**
      * @return \App\Models\User
