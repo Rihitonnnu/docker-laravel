5dd772dc76d2727da37a0b2e16cc717f45d943e7
Rihitonnnu <hexiliang04@gmail.com>
2022-11-07 20:49:03 +0900

    ArticleControllerTestの投稿記事一覧表示のテストケース修正
    以下の問題を解決するためにテストケースを修正しました。
    
    idを指定することで落ちる可能性が考えられるテストケースになっている点
    マジックナンバーを用いた可読性の低いテスト担っている点

--- tests/Feature/User/ArticleControllerTest.php
+++ tests/Feature/User/ArticleControllerTest.php
@@ -5,40 +5,31 @@ namespace Tests\Feature;
 use App\Models\Article;
 use Tests\TestCase;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\Sequence;
 
 class ArticleControllerTest extends TestCase
 {
     protected function setUp(): void
     {
         parent::setUp();
-        $this->user = User::factory()->count(2)
-            ->state(new Sequence(
-                ['id' => 1],//自分
-                ['id' => 2],//他のユーザー
-            ))->create();
+        $this->user = User::factory()->create();
     }
 
     /**
      * @test
      */
-    public function ログインしていれば自分の投稿一覧を表示する()
+    public function ログインしていれば自分が投稿した記事の一覧を表示する()
     {
-        $this->actingAs($this->user[0], 'users');
+        $this->actingAs($this->user, 'users');
 
-        Article::factory()->count(2)
-            ->state(new Sequence(
-                ['user_id' => 1],//自分の記事
-                ['user_id' => 2],//他の人の記事
-            ))->create();
+        $sameUserArticle = Article::factory()->create(['user_id' => $this->user->id]); //自分の投稿分
+        $otherUserArticle = Article::factory()->create(['user_id' => User::factory()->create()->id]); //他のユーザーの投稿分
 
         $response = $this->get(route('user.article.index'));
 
-        $getArticle = Article::where('user_id', $this->user[0]->id)->get();
-
-        $this->assertNotEquals(2, $getArticle[0]->user_id);//自分の記事以外が含まれていないかの確認
-
         $response->assertStatus(200);
+        $response->assertSeeText($sameUserArticle->title);
+        $response->assertDontSeeText($otherUserArticle->title);
+
         $response->assertViewIs('user.article.index');
     }
 
