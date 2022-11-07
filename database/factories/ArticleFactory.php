6a4c528195a637ec31d097227f712ae10314890c
Rihitonnnu <hexiliang04@gmail.com>
2022-11-07 20:48:22 +0900

    ArticleFactoryのuser_id部分を修正

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -18,7 +18,7 @@ class ArticleFactory extends Factory
     public function definition()
     {
         return [
-            'user_id' => $this->faker->numberBetween(1,2),
+            'user_id' => User::factory(),
             'title' => $this->faker->realText(50),
             'content' => $this->faker->realText(400),
         ];
