f485f103f2dc76f65077e6efd2179f93dc48f060
Rihitonnnu <hexiliang04@gmail.com>
2022-11-08 20:08:47 +0900

    Articleモデルの依存解決を行う記述追加

--- app/Http/Controllers/User/ArticleController.php
+++ app/Http/Controllers/User/ArticleController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\Article\PostRequest;
 use App\Models\Article;
 use Illuminate\Support\Facades\Auth;
 
 class ArticleController extends Controller
 {
+    private Article $article;
+
+    public function __construct(Article $article)
+    {
+        $this->article = $article;
+    }
+
     /**
      * @return \Illuminate\Contracts\View\View
      */
