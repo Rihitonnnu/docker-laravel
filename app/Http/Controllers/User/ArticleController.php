e87dfb15dfc4f824b4d4374489d69ddfa8b06b6a
Rihitonnnu <hexiliang04@gmail.com>
2022-11-08 20:09:20 +0900

    create、storeメソッド作成

--- app/Http/Controllers/User/ArticleController.php
+++ app/Http/Controllers/User/ArticleController.php
@@ -23,4 +23,29 @@ class ArticleController extends Controller
     {
         return view('user.article.index', ['articles' => Article::where('user_id', Auth::id())->get()]);
     }
+
+    /**
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function create()
+    {
+        return view('user.article.create');
+    }
+
+    /**
+     * @param PostRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(PostRequest $request)
+    {
+        /** @var int $userId */
+        $userId = Auth::id();
+        /** @var string $title */
+        $title = $request->title;
+        /** @var string $content */
+        $content = $request->content;
+
+        $this->article->storeArticle($userId, $title, $content);
+        return to_route('user.article.index');
+    }
 }
