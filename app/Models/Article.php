d98df018e761fd9caa938eb4fe59c65e06c7e5ec
Rihitonnnu <hexiliang04@gmail.com>
2022-11-09 15:53:10 +0900

    Carbon削除/createメソッドの呼び出しを静的メソッドから->createに変更

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -30,11 +29,10 @@ class Article extends Model
      */
     public function storeArticle(int $userId, string $title, string $content)
     {
-        $article = $this::create([
+        $article = $this->create([
             'user_id' => $userId,
             'title' => $title,
             'content' => $content,
-            'created_at' => Carbon::now(),
         ]);
         return $article;
     }
