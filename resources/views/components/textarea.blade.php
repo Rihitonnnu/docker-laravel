34541df6749cf496b3dd305e3212461f45b63678
Rihitonnnu <hexiliang04@gmail.com>
2022-11-11 11:47:52 +0900

    textareaコンポーネントの一部修正
    編集画面で以前登録していた記事の情報を表示するため$contentを追加しました。また投稿の登録時に$contentは空なのでpropsを用いてnullをデフォルトで渡すように修正しました。

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,1 +1,2 @@
-<textarea id={{ $id }} name={{ $name }} class="w-full h-72 whitespace-pre-wrap bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+@props(['id','name','content'=>null])
+<textarea id={{ $id }} name={{ $name }} class="w-full h-72 whitespace-pre-wrap bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">{{ $content }}</textarea>
