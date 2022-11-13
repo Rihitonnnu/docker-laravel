052dba7120ccb3182a79bf2d8ac664a2d3df9a3d
Rihitonnnu <hexiliang04@gmail.com>
2022-11-14 00:31:24 +0900

    phpdoc書き忘れの修正

--- app/Policies/ArticlePolicy.php
+++ app/Policies/ArticlePolicy.php
@@ -10,21 +10,41 @@ class ArticlePolicy
 {
     use HandlesAuthorization;
 
+    /**
+     * @param User $user
+     * @param Article $article
+     * @return boolean
+     */
     public function show(User $user, Article $article)
     {
         return $user->id == $article->user_id;
     }
 
+    /**
+     * @param User $user
+     * @param Article $article
+     * @return boolean
+     */
     public function edit(User $user, Article $article)
     {
         return $user->id == $article->user_id;
     }
 
+    /**
+     * @param User $user
+     * @param Article $article
+     * @return boolean
+     */
     public function update(User $user, Article $article)
     {
         return $user->id == $article->user_id;
     }
 
+    /**
+     * @param User $user
+     * @param Article $article
+     * @return boolean
+     */
     public function delete(User $user, Article $article)
     {
         return $user->id == $article->user_id;
