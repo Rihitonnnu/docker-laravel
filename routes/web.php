5a284b645e4fad2b47ee2c614ae0ef201deb16c3
Rihitonnnu <hexiliang04@gmail.com>
2022-11-14 21:25:46 +0900

    トップページでindexメソッドを呼び出すようにルーティングを修正

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return to_route('visitor.article.index');
-});
-
-Route::prefix('visitor')->as('visitor.')->group(function () {
-    Route::resource('article', ArticleController::class);
-});
+Route::get('/', [ArticleController::class,'index'])->name('visitor.article.index');
