eac2cc6ece5ef62ec7a1152b748cf036edc80f31
Rihitonnnu <hexiliang04@gmail.com>
2022-11-15 11:29:00 +0900

    showメソッドのテストケースを追加

--- tests/Feature/Visitor/ArticleControllerTest.php
+++ tests/Feature/Visitor/ArticleControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Visitor;
 
 use App\Models\Article;
+use App\Models\User;
 use Tests\TestCase;
 
 class ArticleControllerTest extends TestCase
@@ -17,10 +18,31 @@ class ArticleControllerTest extends TestCase
      */
     public function 投稿一覧を表示()
     {
-        $article=Article::factory()->create();
-        $response=$this->get(route('visitor.article.index'));
+        $article = Article::factory()->create();
+        $response = $this->get(route('visitor.article.index'));
 
         $response->assertSeeText($article->title);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function 投稿詳細を表示()
+    {
+        $user = User::factory()->create(); //表示する投稿の作成者
+        $otherUser = User::factory()->create(); //表示しない投稿の作成者
+
+        $article = Article::factory()->create(['user_id' => $user->id]); //表示する投稿
+        $otherArticle = Article::factory()->create(['user_id' => $otherUser->id]); //表示しない投稿
+
+        $response = $this->get(route('visitor.article.show', ['article' => $article->id, 'userName' => $user->name]));
+
+        $response->assertSeeText($article->title); //表示したい投稿のタイトルが表示されているか
+        $response->assertSeeText($user->name); //表示したい投稿の作成者の名前が表示されているか
+
+        $response->assertDontSeeText($otherArticle->title); //表示しない投稿のタイトルが表示されていないか
+        $response->assertDontSeeText($otherUser->name); //表示しない投稿の作成者の名前が表示されていないか
         $response->assertStatus(200);
     }
 }
