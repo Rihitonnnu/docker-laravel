aed659ac7262d5ac5c743e8e2bec227416cdff06
Rihitonnnu <hexiliang04@gmail.com>
2022-11-15 11:28:18 +0900

    visitorのindexファイルの移動に伴う修正とshowメソッドの追加

--- app/Http/Controllers/Visitor/ArticleController.php
+++ app/Http/Controllers/Visitor/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Visitor;
 
 use App\Http\Controllers\Controller;
 use App\Models\Article;
+use App\Models\User;
 
 class ArticleController extends Controller
 {
@@ -12,6 +13,21 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        return view('visitor.index', ['articles' => Article::orderBy('created_at', 'desc')->paginate(20)]);
+        return view('visitor.article.index', ['articles' => Article::orderBy('created_at', 'desc')->paginate(20)]);
+    }
+
+    /**
+     * @param integer $id
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function show(int $id)
+    {
+        /** @var \App\Models\Article $article */
+        $article = Article::find($id);
+
+        /** @var \App\Models\User $user */
+        $user = User::find($article->user_id);
+
+        return view('visitor.article.show', ['article' => $article, 'userName' => $user->name]); //表示する投稿とその投稿の作成者の名前を取得
     }
 }
