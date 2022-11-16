e1b6be08f0f8fe82af41d1c8818642f92b58048d
Rihitonnnu <hexiliang04@gmail.com>
2022-11-16 15:34:51 +0900

    ユーザー投稿一覧表示のレスポンスや正しく表示されているかのテストケースを追加

---
+++ tests/Feature/Admin/ArticleControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use Tests\TestCase;
+use App\Models\Article;
+use App\Models\User;
+use App\Models\Admin;
+
+class ArticleControllerTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->admin = Admin::factory()->create();
+    }
+
+    /**
+     * ユーザーの投稿と投稿者名の一覧が表示できているか
+     * @test
+     */
+    public function ログインしていればユーザーの投稿一覧を表示()
+    {
+        $this->actingAs($this->admin, 'admins');
+
+        $user = User::factory()->create();
+        $article = Article::factory()->create(['user_id' => $user->id]);
+
+        $response = $this->get(route('admin.article.index'));
+
+        $response->assertSeeText($article->title);
+        $response->assertSeeText($article->user->name);
+        $response->assertStatus(200);
+    }
+}
