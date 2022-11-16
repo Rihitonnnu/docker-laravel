75a44f0e95f0fc607227cd8a9dc34f8b89c28127
Rihitonnnu <hexiliang04@gmail.com>
2022-11-16 12:54:21 +0900

    不要なコメント文削除・テストケース冒頭にテスト内容の概要を記載

--- tests/Feature/Visitor/ArticleControllerTest.php
+++ tests/Feature/Visitor/ArticleControllerTest.php
@@ -26,23 +26,24 @@ class ArticleControllerTest extends TestCase
     }
 
     /**
+     * 投稿詳細表示のレスポンス・表示したい投稿と投稿者名が表示されているか
      * @test
      */
     public function 投稿詳細を表示()
     {
-        $user = User::factory()->create(); //表示する投稿の作成者
-        $otherUser = User::factory()->create(); //表示しない投稿の作成者
+        $user = User::factory()->create();
+        $otherUser = User::factory()->create();
 
-        $article = Article::factory()->create(['user_id' => $user->id]); //表示する投稿
-        $otherArticle = Article::factory()->create(['user_id' => $otherUser->id]); //表示しない投稿
+        $article = Article::factory()->create(['user_id' => $user->id]);
+        $otherArticle = Article::factory()->create(['user_id' => $otherUser->id]);
 
-        $response = $this->get(route('visitor.article.show', ['article' => $article->id, 'userName' => $user->name]));
+        $response = $this->get(route('visitor.article.show', ['article' => $article->id]));
 
-        $response->assertSeeText($article->title); //表示したい投稿のタイトルが表示されているか
-        $response->assertSeeText($user->name); //表示したい投稿の作成者の名前が表示されているか
+        $response->assertSeeText($article->title);
+        $response->assertSeeText($user->name);
 
-        $response->assertDontSeeText($otherArticle->title); //表示しない投稿のタイトルが表示されていないか
-        $response->assertDontSeeText($otherUser->name); //表示しない投稿の作成者の名前が表示されていないか
+        $response->assertDontSeeText($otherArticle->title);
+        $response->assertDontSeeText($otherUser->name);
         $response->assertStatus(200);
     }
 }
