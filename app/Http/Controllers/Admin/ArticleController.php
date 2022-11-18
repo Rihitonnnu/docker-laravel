de1eb0cf961d0d7523d3e18699286ea5931b1787
Rihitonnnu <hexiliang04@gmail.com>
2022-11-18 09:40:47 +0900

    showメソッド追加

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -8,10 +8,24 @@ use App\Models\Article;
 class ArticleController extends Controller
 {
     /**
+     * 投稿一覧を表示
      * @return \Illuminate\Contracts\View\View
      */
     public function index()
     {
         return view('admin.article.index', ['articles' => Article::with('user')->orderBy('created_at', 'desc')->paginate(20)]);
+    }
+
+    /**
+     * 投稿詳細を表示
+     * @param integer $id
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function show(int $id)
+    {
+        /** @var \App\Models\Article $article */
+        $article = Article::with('user')->where('id', $id)->first();
+
+        return view('admin.article.show', ['article' => $article]);
     }
 }
