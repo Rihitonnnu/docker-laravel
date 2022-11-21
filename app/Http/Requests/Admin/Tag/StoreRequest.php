fa7f77769557175f43ca54ad5bc25595e9ccfe35
Rihitonnnu <hexiliang04@gmail.com>
2022-11-21 17:41:29 +0900

    タグ作成のバリデーションを作成

---
+++ app/Http/Requests/Admin/Tag/StoreRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Admin\Tag;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:15',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'タグ名を入力して下さい',
+            'name.max' => 'タグ名は15文字以内で入力してください',
+        ];
+    }
+}
