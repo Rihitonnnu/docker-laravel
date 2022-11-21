a6cda0234830d6fc39d1d54d47186b527b33422e
Rihitonnnu <hexiliang04@gmail.com>
2022-11-21 17:38:05 +0900

    storeメソッド追加

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -3,15 +3,35 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Tag\StoreRequest;
 use App\Models\Tag;
 
 class TagController extends Controller
 {
+    private Tag $tag;
+
+    public function __construct(Tag $tag)
+    {
+        $this->tag = $tag;
+    }
+
     /**
      * @return \Illuminate\Contracts\View\View
      */
     public function index()
     {
-        return view('admin.tag.index', ['tags' => Tag::paginate(20)]);
+    }
+
+    /**
+     * @param StoreRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(StoreRequest $request)
+    {
+        /** @var string $name */
+        $name = $request->input('name');
+        $this->tag->storeTag($name);
+
+        return to_route('admin.tag.index');
     }
 }
