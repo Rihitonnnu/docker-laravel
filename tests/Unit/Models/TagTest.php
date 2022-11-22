a0b4979f62091d04bbe5aa1c2ec51aa485afea30
Rihitonnnu <hexiliang04@gmail.com>
2022-11-22 13:01:57 +0900

    タグのDB更新処理のテストケースを追加

--- tests/Unit/Models/TagTest.php
+++ tests/Unit/Models/TagTest.php
@@ -7,10 +7,25 @@ use Tests\TestCase;
 
 class TagTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->tag = Tag::factory()->create([
+            'name' => 'ほげほげ',
+        ]);
+    }
+
     public function test_storeTag()
     {
         $tag = (new Tag())->storeTag('ほげほげ');
 
         $this->assertEquals('ほげほげ', $tag->name);
+    }
+
+    public function test_updateTag()
+    {
+        $updateTag = (new Tag())->updateTag('ふがふが', $this->tag->id);
+
+        $this->assertEquals('ふがふが', $updateTag->name);
     }
 }
