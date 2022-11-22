2954207b4b6b3aed53d4dc5ccbfca43449c2b89a
Rihitonnnu <hexiliang04@gmail.com>
2022-11-22 12:59:57 +0900

    editとupdateメソッドを追加

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Tag\StoreRequest;
+use App\Http\Requests\Admin\Tag\UpdateRequest;
 use App\Models\Tag;
 
 class TagController extends Controller
@@ -33,6 +34,29 @@ class TagController extends Controller
         $name = $request->input('name');
         $this->tag->storeTag($name);
 
+        return to_route('admin.tag.index');
+    }
+
+    /**
+     * @param integer $id
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function edit(int $id)
+    {
+        return view('admin.tag.edit', ['tag' => Tag::find($id)]);
+    }
+
+    /**
+     * @param UpdateRequest $request
+     * @param integer $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(UpdateRequest $request, int $id)
+    {
+        /** @var string $name */
+        $name = $request->name;
+
+        $this->tag->updateTag($name, $id);
         return to_route('admin.tag.index');
     }
 }
