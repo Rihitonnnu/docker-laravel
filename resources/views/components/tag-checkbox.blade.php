dc915932b8c5284fe4658123ceff662a10fb8c97
Rihitonnnu <hexiliang04@gmail.com>
2022-11-22 17:43:41 +0900

    投稿作成で表示するタグのチェックボックスのbladeコンポーネントを作成

---
+++ resources/views/components/tag-checkbox.blade.php
@@ -0,0 +1,4 @@
+<div class="flex mt-2">
+    <input type="checkbox" id="tag" name="tag[]" class="w-5 h-5 text-blue-600 bg-gray-100 rounded border-gray-300 mt-2" value="{{ $value }}">
+    <label for="checkbox" class="pt-2 ml-1 mr-3 w-full text-sm text-gray-900 dark:text-gray-300">{{ $name }}</label>
+</div>
