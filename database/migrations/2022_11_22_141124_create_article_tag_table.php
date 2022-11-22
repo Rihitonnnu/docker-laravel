d677a90ed14f3efce2e86dd620d8395d3b5da4da
Rihitonnnu <hexiliang04@gmail.com>
2022-11-22 17:39:26 +0900

    articleとtagの中間テーブルを作成

---
+++ database/migrations/2022_11_22_141124_create_article_tag_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Article;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Psy\CodeCleaner\FunctionReturnInWriteContextPass;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('article_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article_tag');
+    }
+};
