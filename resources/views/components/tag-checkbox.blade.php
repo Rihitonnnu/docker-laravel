45183e8548a57c0a3287bab10acb19fdd898c907
Rihitonnnu <hexiliang04@gmail.com>
2022-11-23 15:14:30 +0900

    バリデーションがかかった時にチェックボックスの内容を保持するように修正

--- resources/views/components/tag-checkbox.blade.php
+++ resources/views/components/tag-checkbox.blade.php
@@ -1,4 +1,7 @@
 <div class="flex mt-2">
-    <input type="checkbox" id="tag" name="tag[]" class="w-5 h-5 text-blue-600 bg-gray-100 rounded border-gray-300 mt-2" value="{{ $value }}">
-    <label for="checkbox" class="pt-2 ml-1 mr-3 w-full text-sm text-gray-900 dark:text-gray-300">{{ $name }}</label>
+    <input type="checkbox" id="tags" name="tags[]"
+        class="w-5 h-5 text-blue-600 bg-gray-100 rounded border-gray-300 mt-2" value="{{ $id }}"
+        @if (is_array(old('tags')) && in_array($id, old('tags'))) checked @endif />
+    <label for="checkbox"
+        class="pt-2 ml-1 mr-3 w-full text-sm text-gray-900 dark:text-gray-300">{{ $name }}</label>
 </div>
