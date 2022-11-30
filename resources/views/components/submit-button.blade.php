17e50d9bc642c7aeadd6f181a1ae78d9eac914b3
Rihitonnnu <hexiliang04@gmail.com>
2022-11-30 11:15:26 +0900

    閉じタグがaタグになっていたので修正

--- resources/views/components/submit-button.blade.php
+++ resources/views/components/submit-button.blade.php
@@ -1,1 +1,2 @@
-<button type="submit" {{ $attributes->merge(['class' => 'flex mx-auto justify-center text-white border-0 py-2 px-6 focus:outline-none rounded']) }}>{{ $title }}</a>
+<button type="submit"
+    {{ $attributes->merge(['class' => 'flex mx-auto justify-center text-white border-0 py-2 px-6 focus:outline-none rounded']) }}>{{ $title }}</button>
