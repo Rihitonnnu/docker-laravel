ef8d5a452fac833040f14c3c784203471be45269
Rihitonnnu <hexiliang04@gmail.com>
2022-11-30 11:13:11 +0900

    検索のバリデーション作成

---
+++ app/Http/Requests/SearchRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SearchRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'keyword' => 'required|max:15',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'keyword.required' => '検索ワードを入力して下さい',
+            'keyword.max' => '15文字以下で入力して下さい',
+        ];
+    }
+}
