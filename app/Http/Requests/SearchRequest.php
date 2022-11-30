c9f5332481d1e2e778a91ad091b81dd5122c4f50
Rihitonnnu <hexiliang04@gmail.com>
2022-11-30 13:42:55 +0900

    バリデーション時のリダイレクト先を変更

--- app/Http/Requests/SearchRequest.php
+++ app/Http/Requests/SearchRequest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class SearchRequest extends FormRequest
 {
+    protected $redirectRoute='visitor.article.index';
+    
     /**
      * Determine if the user is authorized to make this request.
      *
