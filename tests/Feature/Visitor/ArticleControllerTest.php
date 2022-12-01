c70b8e4841697fc7e625c1c9056fb8e9a40f94df
Rihitonnnu <hexiliang04@gmail.com>
2022-12-01 11:34:07 +0900

    検索処理のテストケース作成・別テストケース不要な記述削除

--- tests/Feature/Visitor/ArticleControllerTest.php
+++ tests/Feature/Visitor/ArticleControllerTest.php
@@ -46,7 +46,6 @@ class ArticleControllerTest extends TestCase
         $article = Article::factory()->create(['user_id' => $user->id]);
         $otherArticle = Article::factory()->create(['user_id' => $otherUser->id]);
 
-        $otherTag = Tag::factory()->create();
         $article->tags()->sync([$this->tag->id]);
 
         $response = $this->get(route('visitor.article.show', ['article' => $article->id]));
@@ -58,5 +57,23 @@ class ArticleControllerTest extends TestCase
         $response->assertDontSeeText($otherArticle->title);
         $response->assertDontSeeText($otherUser->name);
         $response->assertStatus(200);
+    }
+
+    /**
+     * 検索した投稿のタイトルが表示され、検索していないものは表示されていないか
+     * @test
+     */
+    public function 投稿の検索結果を表示()
+    {
+        $tag = Tag::factory()->create(['name' => 'ほげほげ']);
+        $article = Article::factory()->create();
+        $otherArticle = Article::factory()->create();
+        $article->tags()->sync([$tag->id]);
+
+        $response=$this->get(route('visitor.article.search',['keyword' => 'ほげほげ']));
+
+        $response->assertStatus(200);
+        $response->assertSeeText($article->title);
+        $response->assertDontSeeText($otherArticle->title);
     }
 }
